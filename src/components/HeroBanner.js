03ea884be5abd4666f804363fc0bcad26a80c702
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-09-20 10:57:31 +0100

    search

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import { Box,Stack,Typography } from '@mui/material'
+import { Box,Stack,Typography,Button } from '@mui/material'
+import he from '../assets/he.jpg'
+import { CssBaseline } from '@material-ui/core';
 
 const HeroBanner = () => {
   return (
-    <Box sx={{ mt:{ lg:'212px',xs:'70px'}, ml:{sm:'50px'}}} position="relative" p="20px" >
-        <Typography color="#FF2625" fontWeight="600"  fontsize="26px">
-            Fitness
-        </Typography>
-        <Typography>
-            Sweat,Smile <br/>
-            Repeat !
-        </Typography>
-        <Typography>
-            Check out the most effective Exercises !
-        </Typography>
+    <Box sx={{ mt: { lg: '100px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+    “You shall gain, but you shall pay with sweat, blood, and vomit.”  <br />
+    Pavel Tsatsouline
+    </Typography>
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+      Check out the most effective exercises personalized to you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.8', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Exercise
+    </Typography>
+    
     </Box>
   )
 }
--- src/components/SearchExercise.js
+++ src/components/SearchExercise.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react';
+import {Box,Stack,Button,TextField,Typography} from '@mui/material';
 
 const SearchExercise = () => {
+  const [search, setSearch] = useState('');
+  const handleSearch =async () => {
+    if (search) {
+      
+    }
+  }
   return (
-    <div>SearchExercise</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography  color="#FF2625" fontWeight={700}sx={{fontSize:{lg:'44px',xs:'30px'}}} mb="50px" textAlign="center">
+exercices you should know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField sx={{ input:{ fontWeight:'700',border:'none',borderRadius:'40px'},width:{lg:'1000px', xs:'350px'}, backgroundColor:'rgba(255, 255, 255, .7)',borderRadius:'10px',}} height="76px" value={search} onChange={(e) =>setSearch(e.target.value.toLocaleLowerCase())} placeholder="Search Exercices" type="text"/>
+        <Button className="search-btn" sx={{bgcolor:'#FF2625',color:'#fff',textTransform:'none',width:{lg:'175px',xs:'80px'},fontSize:{lg:'20px',xs:'14px'},height:'56px',position:'absolute',right:'0'}} onClick={handleSearch}>
+          search
+        </Button>
+      </Box>
+      
+    </Stack>
   )
 }
 
