ece7201a9a14c723b22bd0660ef8a6ea81a393df
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-10-07 19:19:19 +0100

    d

--- src/components/LandingPage/Coaches/Coaches.js
+++ src/components/LandingPage/Coaches/Coaches.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Coaches.css';
 import {CoachesData} from '../../LandingPage/Data/CoachesData';
+import leftArrow from '../../../assets/arrow-left.png';
+import rightArrow from '../../../assets/arrow-right.png';
 const Coaches = () => {
+  const [selected,setSelected]=useState(0);
+  const tLength=CoachesData.length;
   return (
     <div className='Coaches'>
       <div className='left-c'>
         <span>Coaches</span>
         <span className='stroke-text'>what</span>
         <span>they will <span className='stroke-text'>add</span> </span>
-        <span>{CoachesData[0].review }</span>
+        <span>{CoachesData[selected].review }</span>
+        <span>
+        <span style={{color:'var(--orange)'}}>{CoachesData[selected].name }</span>{" "}
+        -{CoachesData[selected].status }
+        </span>
       </div>
-      <div className='right-c'></div>
+      <div className='right-c'>
+        <img src={CoachesData[selected].image } />
+        <div className='arrow'>
+          <img src={leftArrow } />
+          <img src={rightArrow } />
+        </div>
+      </div>
     </div>
   )
 }
