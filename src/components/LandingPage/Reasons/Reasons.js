38cf7a1a157e02e372d068651e908866a3e3f73c
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-10-07 02:39:40 +0100

    8

--- src/components/LandingPage/Reasons/Reasons.js
+++ src/components/LandingPage/Reasons/Reasons.js
@@ -4,6 +4,10 @@ import image1 from '../../../assets/image1.png';
 import image2 from '../../../assets/image2.png';
 import image3 from '../../../assets/image3.png';
 import image4 from '../../../assets/image4.png';
+import barbell from '../../../assets/barbell.png';
+import coach from '../../../assets/coach.png';
+import salad from '../../../assets/salad.png';
+import supplement from '../../../assets/supplement.png';
 const Reasons = () => {
   return (
     <div className='Reasons' >
@@ -13,7 +17,38 @@ const Reasons = () => {
             <img src={image3} alt={image3} />
             <img src={image4} alt={image4} />
         </div>
-        <div className='right-r'> right side</div>
+        <div className='right-r'>
+          <span>Some Reasons</span>
+          <div>
+            <span className='stroke-text'>Why </span>
+            <span> Choose Us ?</span>
+          </div>
+
+          
+            <div className='details-r'>
+            <div>
+              <img src={barbell} />
+              <span>best exercises selected</span>
+            </div>
+
+            <div>
+              <img src={salad} />
+              <span>infos about food</span>
+            </div>
+
+            <div>
+              <img src={coach} />
+              <span>over 20 coach suggested </span>
+            </div>
+
+            <div>
+              <img src={supplement} />
+              <span>supps</span>
+            </div>
+
+          </div>
+
+        </div>
     </div>
   )
 }
