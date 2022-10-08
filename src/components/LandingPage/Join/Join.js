516e8dc0cec092e83cc11eff8fa9f600ca230b27
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-10-08 09:10:48 +0100

    -footer

--- src/components/LandingPage/Join/Join.js
+++ src/components/LandingPage/Join/Join.js
@@ -1,7 +1,20 @@
-import { Button } from '@mui/material'
-import React from 'react'
+
+import React,{useRef} from 'react';
 import './Join.css'
+import emailjs from '@emailjs/browser';
 const Join = () => {
+    const form=useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_h53rag2', 'template_ui99j4n', form.current, '1i4okjbvqpSGAgxsk')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
   return (
     <div className='Join'>
         <div className='left-j'>
@@ -18,9 +31,9 @@ const Join = () => {
 
         </div>
         <div className='right-j'>
-            <form className='email'>
-                <input type='email' placeholder='Enter your Email address '/>
-                <Button className='btn btn-j'>Join Now</Button>
+            <form ref={form} className='email' onSubmit={sendEmail}>
+                <input type='email' name="user_email" placeholder='Enter your Email address '/>
+                <button className='btn btn-j'>Join Now</button>
             </form>
         </div>
     </div>
